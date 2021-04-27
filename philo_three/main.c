4e834679aef3286f55dbac1bdc6a727145455d91
khermann <mihchepi@gmail.com>
2021-04-27 14:22:40 +0300

    first

---
+++ philo_three/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khermann <khermann@21-school.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 13:30:13 by khermann          #+#    #+#             */
+/*   Updated: 2021/04/27 14:21:39 by khermann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+int	main(int argc, char **argv)
+{
+
+	return (0);
+}
